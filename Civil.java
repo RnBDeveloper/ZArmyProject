f1c49cafaaa1942273d671168894447ed11a9c8b
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2019-01-17 16:32:32 +0100

    Create Civil.java

---
+++ Civil.java
@@ -0,0 +1,10 @@
+import com.ZArmy.behavior.*;
+
+public class Civil extends Person {
+	public Civil() {
+	}
+
+	public Civil(FightingSpirit spirit, Movement mov, MedicalCare medicalCare) {
+		super(spirit, mov, medicalCare);
+	}
+}
