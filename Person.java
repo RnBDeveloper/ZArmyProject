694be730a0fa4c8a817e747548b3c3fa075aa766
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2019-01-22 15:44:40 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,6 +1,11 @@
 import com.ZArmy.behavior.*;
 
 public abstract class Person {
+	/*We don't add "implements FightingSpirit, Movement, MedicalCare"
+	*to the Class Person declaration since three of those classes are abstract as well,
+	*& it's their concrete subclasses (e.g. GunFight, Run, FirstAid, etc) that will contain
+	*their concrete (implemented) methods.
+	*/
 	protected FightingSpirit fightingSpirit = new Pacifist();
 	protected Movement movement = new Walk();
 	protected MedicalCare medicalCare = new NoMedicalCare();
